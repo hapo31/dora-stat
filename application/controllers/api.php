98e1e255afbc312b6922c546a5f0829989f64812
happou31 <happo31@gmail.com>
2016-10-02 22:32:42 +0900

    コントローラー作成

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -32,6 +32,49 @@ class api extends CI_Controller {
              ->set_content_type('application/json')
              ->set_output(json_encode($result));
     }
+
+    public function stat() {
+        $fromDate = 0;
+        $toDate = 0;
+
+        if( !is_null($this->input->get('date')) ) {
+            $day = new Datetime($this->input->get('date'));
+            $fromDate = $day->format("Y-m")."-01 00:00:00";
+            $toDate = $day->format("Y-m-t")." 23:59:59";
+        } else if(
+            !is_null($this->input->get('from')) && 
+            !is_null($this->input->get('to'))
+        ){
+            $from = new Datetime($this->input->get('from'));
+            $to   = new Datetime($this->input->get('to'));
+
+            $fromDate = $from->format("Y-m")."-01 00:00:00";
+            $toDate   = $to->format("Y-m-t")." 23:59:59";
+        }
+        else {
+            $this->output->set_status_header(400);
+            $this->output
+                ->set_content_type('application/json')
+                ->set_output(json_encode(array('error' => "require paramerter 'date' or ('from','to') ")));
+            return ;
+        }
+        // $this->output
+        //         ->set_content_type('application/json')
+        //         ->set_output(json_encode( array($fromDate, $toDate) ));
+
+        if(is_null($fromDate) || is_null($toDate)){
+            $this->output->set_status_header(400);
+            $this->output
+                ->set_content_type('application/json')
+                ->set_output(json_encode(array('error' => 'date format is null or invalid.')));
+            return ;
+        }
+        $result = $this->dao->stat_boron_count($fromDate, $toDate);
+        $this->output
+                ->set_content_type('application/json')
+                ->set_output(json_encode( $result ));
+     
+    }
 }
 
 ?>
--- application/models/dao.php
+++ application/models/dao.php
@@ -7,11 +7,33 @@ class dao extends CI_Model {
     }
     
     public function days($from, $to) {
-        $this->db->select('*');
+        $this->db->select('count(is_boron = 1) as `count`, tweet');
         $this->db->where('date >', $from);
         $this->db->where('date <', $to);
 
         return $this->db->get('daily')->result_array();
     }
+//         SELECT 
+// DATE_FORMAT(date, "%Y/%m") as `date` , count(*) as tweets , count(is_boron = 1) as boron_count
+// FROM `daily`
+// GROUP BY DATE_FORMAT(date, "%Y/%m") 
+    public function stat_boron_count($from, $to) {
+        $sql = <<<EOD
+select DATE_FORMAT(date, "%Y/%m") as `date`, count(*) as tweet_count ,count(is_boron = 1) as boron_count
+from daily
+where is_boron = 1 and
+date > ? and date < ? 
+group by DATE_FORMAT(date, "%Y/%m")
+EOD;
+        return $this->db->query($sql, array($from, $to) )->result_array();
+
+        // $this->db->select('DATE_FORMAT(date, "%Y/%m") as `date`, count(*) as boron_count');
+        // $this->db->where('is_boron', 1);
+        // $this->db->where('date >', $from);
+        // $this->db->where('date <', $to);
+        
+        // return $this->db->get('daily')->result_array();
+    }
+
 }
 ?>
